81f5f0c7c9c504bc5129c7ef36dc1d573b906cea
Sheldon Lai <ie925155@gmail.com>
2022-04-18 23:05:45 +0800

    add char test case

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -3,7 +3,36 @@
  * CS 241 - Fall 2019
  */
 #include "vector.h"
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+
+static void Test_char_vector(void) {
+    vector *v = char_vector_create();
+
+    char *t = "abcdef";
+    for (size_t i = 0; i < strlen(t); i++) {
+        vector_push_back(v, &t[i]);
+    }
+    assert(vector_size(v) == 6);
+
+    vector_pop_back(v);
+    assert(vector_size(v) == 5);
+
+    int i = 0;
+    for (void **it = vector_begin(v); it != vector_end(v); ++it) {
+        assert(t[i++] == (char)(*(char *)*it));
+        //printf("%p, %c\n", *it, (char)(*(char *)*it));
+    }
+
+    vector_destroy(v);
+    printf("passed %s\n", __FUNCTION__);
+}
+
 int main(int argc, char *argv[]) {
-    // Write your test cases here
+
+    Test_char_vector();
+
     return 0;
 }
