25161afdffb4b45e2e30823821dfe08af29b4ddd
Sheldon Lai <ie925155@gmail.com>
2022-04-24 10:36:33 +0800

    add float vector testcase

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -153,7 +153,82 @@ static void Test_double_vector(void) {
     assert(vector_size(v) == 0);
 
     vector_destroy(v);
-    
+
+    printf(ANSI_COLOR_GREEN "passed" ANSI_COLOR_RESET "\n");
+}
+
+
+static void Test_float_vector(void) {
+    printf("%s -> ", __FUNCTION__);
+
+    vector *v = float_vector_create();
+
+    float t[] = {1.2, 2.4, 3.6, 4.8, 5.0, 6.7, 7.9, 8.178, 9.44, 10.0};
+    for (size_t i = 0; i < sizeof(t) / sizeof(t[0]); i++) {
+       vector_push_back(v, &t[i]);
+    }
+    assert(vector_size(v) == 10);
+
+    vector_pop_back(v);
+    assert(vector_size(v) == 9);
+
+    int i = 0;
+    for (void **it = vector_begin(v); it != vector_end(v); ++it) {
+        assert(t[i++] == (float)(*(float *)*it));
+        //printf("%p, %c\n", *it, (float)(*(float *)*it));
+    }
+
+    vector_resize(v, 20);
+    assert(vector_size(v) == 20);
+
+    vector_resize(v, 5);
+    assert(vector_size(v) == 5);
+
+    vector_reserve(v, 30);
+    assert(vector_capacity(v) >= 30);
+
+    assert(vector_empty(v) == false);
+
+    void **it;
+    it = vector_at(v, 0);
+    assert(t[0] == (float)(*(float *)*it));
+    it = vector_at(v, 1);
+    assert(t[1] == (float)(*(float *)*it));
+    it = vector_at(v, 2);
+    assert(t[2] == (float)(*(float *)*it));
+    it = vector_at(v, 3);
+    assert(t[3] == (float)(*(float *)*it));
+
+    float value = 9.99;
+    vector_set(v, 0, &value);
+    float *a = (float *)vector_get(v, 0);
+    assert(*a == value);
+
+    a = (float *)vector_get(v, 100);
+    assert(a == NULL);
+
+    it = vector_front(v);
+    assert(value == (float)(*(float *)*it));
+
+    it = vector_back(v);
+    assert(t[vector_size(v)-1] == (float)(*(float *)*it));
+
+    value = 8.88;
+    vector_insert(v, 1, &value);
+    assert(*(float *)vector_get(v, 1) == value);
+    assert(*(float *)vector_get(v, 0) == (float)9.99);
+    assert(*(float *)vector_get(v, 2) == (float)2.4);
+
+    vector_erase(v, 1);
+    assert(*(float *)vector_get(v, 0) == (float)9.99);
+    assert(*(float *)vector_get(v, 1) == (float)2.4);
+    assert(*(float *)vector_get(v, 2) == (float)3.6);
+
+    vector_clear(v);
+    assert(vector_size(v) == 0);
+
+    vector_destroy(v);
+
     printf(ANSI_COLOR_GREEN "passed" ANSI_COLOR_RESET "\n");
 }
 
@@ -162,6 +237,8 @@ int main(int argc, char *argv[]) {
     Test_char_vector();
 
     Test_double_vector();
+
+    Test_float_vector();
 
     return 0;
 }
