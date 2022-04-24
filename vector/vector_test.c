317e5ad9c40b9f4398060623f6f960a782bcc46e
Sheldon Lai <ie925155@gmail.com>
2022-04-24 10:20:54 +0800

    add double vector testcase

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -7,8 +7,12 @@
 #include <string.h>
 #include <assert.h>
 
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 static void Test_char_vector(void) {
+    printf("%s -> ", __FUNCTION__);
+
     vector *v = char_vector_create();
 
     char *t = "abcdefghij";
@@ -58,7 +62,7 @@ static void Test_char_vector(void) {
     assert('s' == (char)(*(char *)*it));
 
     it = vector_back(v);
-    assert('e' == (char)(*(char *)*it));
+    assert(t[vector_size(v)-1] == (char)(*(char *)*it));
 
     vector_insert(v, 1, "a");
     assert(*(char *)vector_get(v, 1) == 'a');
@@ -74,12 +78,90 @@ static void Test_char_vector(void) {
     assert(vector_size(v) == 0);
 
     vector_destroy(v);
-    printf("passed %s\n", __FUNCTION__);
+
+    printf(ANSI_COLOR_GREEN "passed" ANSI_COLOR_RESET "\n");
+}
+
+
+static void Test_double_vector(void) {
+    printf("%s -> ", __FUNCTION__);
+
+    vector *v = double_vector_create();
+
+    double t[] = {1.2, 2.4, 3.6, 4.8, 5.0, 6.7, 7.9, 8.178, 9.44, 10.0};
+    for (size_t i = 0; i < sizeof(t) / sizeof(t[0]); i++) {
+       vector_push_back(v, &t[i]);
+    }
+    assert(vector_size(v) == 10);
+
+    vector_pop_back(v);
+    assert(vector_size(v) == 9);
+
+    int i = 0;
+    for (void **it = vector_begin(v); it != vector_end(v); ++it) {
+        assert(t[i++] == (double)(*(double *)*it));
+        //printf("%p, %c\n", *it, (double)(*(double *)*it));
+    }
+
+    vector_resize(v, 20);
+    assert(vector_size(v) == 20);
+
+    vector_resize(v, 5);
+    assert(vector_size(v) == 5);
+
+    vector_reserve(v, 30);
+    assert(vector_capacity(v) >= 30);
+
+    assert(vector_empty(v) == false);
+
+    void **it;
+    it = vector_at(v, 0);
+    assert(t[0] == (double)(*(double *)*it));
+    it = vector_at(v, 1);
+    assert(t[1] == (double)(*(double *)*it));
+    it = vector_at(v, 2);
+    assert(t[2] == (double)(*(double *)*it));
+    it = vector_at(v, 3);
+    assert(t[3] == (double)(*(double *)*it));
+
+    double value = 9.99;
+    vector_set(v, 0, &value);
+    double *a = (double *)vector_get(v, 0);
+    assert(*a == value);
+
+    a = (double *)vector_get(v, 100);
+    assert(a == NULL);
+
+    it = vector_front(v);
+    assert(value == (double)(*(double *)*it));
+
+    it = vector_back(v);
+    assert(t[vector_size(v)-1] == (double)(*(double *)*it));
+
+    value = 8.88;
+    vector_insert(v, 1, &value);
+    assert(*(double *)vector_get(v, 1) == value);
+    assert(*(double *)vector_get(v, 0) == 9.99);
+    assert(*(double *)vector_get(v, 2) == 2.4);
+
+    vector_erase(v, 1);
+    assert(*(double *)vector_get(v, 0) == 9.99);
+    assert(*(double *)vector_get(v, 1) == 2.4);
+    assert(*(double *)vector_get(v, 2) == 3.6);
+
+    vector_clear(v);
+    assert(vector_size(v) == 0);
+
+    vector_destroy(v);
+    
+    printf(ANSI_COLOR_GREEN "passed" ANSI_COLOR_RESET "\n");
 }
 
 int main(int argc, char *argv[]) {
 
     Test_char_vector();
+
+    Test_double_vector();
 
     return 0;
 }
