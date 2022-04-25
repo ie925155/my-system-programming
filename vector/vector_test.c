4f5cb3c371104cdb6b99f38ebd430bdb732a7d51
Sheldon Lai <ie925155@gmail.com>
2022-04-25 23:18:28 +0800

    add shallow test case

--- vector/vector_test.c
+++ vector/vector_test.c
@@ -382,6 +382,78 @@ static void Test_string_vector(void) {
     printf(ANSI_COLOR_GREEN "passed" ANSI_COLOR_RESET "\n");
 }
 
+static void Test_shallow_vector(void) {
+    printf("%s -> ", __FUNCTION__);
+
+    vector *v = shallow_vector_create();
+
+    char *t = "abcdefghij";
+    for (size_t i = 0; i < strlen(t); i++) {
+        vector_push_back(v, &t[i]);
+    }
+    assert(vector_size(v) == 10);
+
+    vector_pop_back(v);
+    assert(vector_size(v) == 9);
+
+    int i = 0;
+    for (void **it = vector_begin(v); it != vector_end(v); ++it) {
+        assert(t[i++] == (char)(*(char *)*it));
+        //printf("%p, %c\n", *it, (char)(*(char *)*it));
+    }
+
+    vector_resize(v, 20);
+    assert(vector_size(v) == 20);
+
+    vector_resize(v, 5);
+    assert(vector_size(v) == 5);
+
+    vector_reserve(v, 30);
+    assert(vector_capacity(v) >= 30);
+
+    assert(vector_empty(v) == false);
+
+    void **it;
+    it = vector_at(v, 0);
+    assert(t[0] == (char)(*(char *)*it));
+    it = vector_at(v, 1);
+    assert(t[1] == (char)(*(char *)*it));
+    it = vector_at(v, 2);
+    assert(t[2] == (char)(*(char *)*it));
+    it = vector_at(v, 3);
+    assert(t[3] == (char)(*(char *)*it));
+
+    vector_set(v, 0, "s");
+    char *a = (char *)vector_get(v, 0);
+    assert(*a == 's');
+
+    a = (char *)vector_get(v, 100);
+    assert(a == NULL);
+
+    it = vector_front(v);
+    assert('s' == (char)(*(char *)*it));
+
+    it = vector_back(v);
+    assert(t[vector_size(v)-1] == (char)(*(char *)*it));
+
+    vector_insert(v, 1, "a");
+    assert(*(char *)vector_get(v, 1) == 'a');
+    assert(*(char *)vector_get(v, 0) == 's');
+    assert(*(char *)vector_get(v, 2) == 'b');
+
+    vector_erase(v, 1);
+    assert(*(char *)vector_get(v, 0) == 's');
+    assert(*(char *)vector_get(v, 1) == 'b');
+    assert(*(char *)vector_get(v, 2) == 'c');
+
+    vector_clear(v);
+    assert(vector_size(v) == 0);
+
+    vector_destroy(v);
+    
+    printf(ANSI_COLOR_GREEN "passed" ANSI_COLOR_RESET "\n");
+}
+
 int main(int argc, char *argv[]) {
 
     Test_char_vector();
@@ -393,6 +465,8 @@ int main(int argc, char *argv[]) {
     Test_int_vector();
 
     Test_string_vector();
+
+    Test_shallow_vector();
 
     return 0;
 }
