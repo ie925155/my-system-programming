016c7115b24bc0ec193d6d8a60c191e4d5dd1ee4
Sheldon Lai <ie925155@gmail.com>
2022-06-03 10:57:15 +0800

    shell support logica operator &&, ||, ;

--- shell/shell.c
+++ shell/shell.c
@@ -23,6 +23,8 @@ typedef struct process {
     pid_t pid;
 } process;
 
+static vector *split_cmd_args(const char *cmd);
+
 static bool is_built_in(const char *cmd) {
     if (strcmp(cmd, "cd") == 0) {
         return true;
@@ -36,21 +38,65 @@ static bool is_built_in(const char *cmd) {
     return false;
 }
 
-static bool is_need_record(const char *cmd) {
+static bool is_need_record(const char *cmd_with_args) {
+    char cmd[32] = {0x0};
+    vector *v = split_cmd_args(cmd_with_args);
+    memcpy(cmd, vector_get(v, 0), strlen(vector_get(v, 0)));
+    vector_destroy(v);
     return (!is_built_in(cmd) || (is_built_in(cmd) && (strcmp(cmd, "cd") == 0)));
 }
 
-static vector *parse(const char *line)
-{
-    sstring *s = cstr_to_sstring(line);
+static vector *split_cmd_args(const char *cmd) {
+    sstring *s = cstr_to_sstring(cmd);
     sstring_substitute(s, 0, "\n", "");
     vector *v = sstring_split(s, ' ');
     sstring_destroy(s);
     return v;
 }
 
-static void run_command(vector *v, char **next)
+static char *trimwhitespace(char *str)
 {
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
+}
+
+static vector *parse(const char *line)
+{
+#define INPUT_COMMAND_LIMIT (2)
+    vector *cmds = string_vector_create();
+    char *str = strdup(line);
+    char *pch;
+    pch = strtok(str, "&&||;");
+    int count = 0;
+    // only support x && y, x || y, x; y
+    while (pch != NULL) {
+        vector_push_back(cmds, trimwhitespace(pch));
+        if (++count >= INPUT_COMMAND_LIMIT) {
+            break;
+        }
+        pch = strtok(NULL, "&&||;");
+    }
+    free(str);
+    return cmds;
+}
+
+static int32_t run_command(vector *v, char **next)
+{
+    int32_t ret = 0;
     const char *cmd = vector_get(v, 0);
     if (is_built_in(cmd)) {
         if (strcmp(cmd, "cd") == 0) {
@@ -58,7 +104,7 @@ static void run_command(vector *v, char **next)
             getcwd(cwd, sizeof(cwd));
             char *path = (char *)vector_get(v, 1);
             sprintf(str, "%s/%s", cwd, path);
-            int ret = (path != NULL) ? chdir(str) : chdir(getenv("HOME"));
+            ret = (path != NULL) ? chdir(str) : chdir(getenv("HOME"));
             if (ret != 0)
                 fprintf(stderr, "%s: No such file or directory\n", str);
         } else if (strcmp(cmd, "!history") == 0) {
@@ -107,9 +153,9 @@ static void run_command(vector *v, char **next)
                 fprintf(stderr, "%s\n", *next);
             }
         }
-        return;
+        return ret;
     }
-    int pid = fork();
+    pid_t pid = fork();
     if (pid == -1) {
         print_fork_failed();
         exit(1);
@@ -128,11 +174,19 @@ static void run_command(vector *v, char **next)
         exit(1);
     } else {
         int status = 0;
-        waitpid(pid, &status, 0);
+        pid = waitpid(pid, &status, 0);
+        if (pid != -1 && WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            //printf("process %d returned %d\n", pid, exit_status);
+            ret = exit_status;
+        }
     }
+
+    return ret;
 }
 
 static void sig_handler(int signum) {
+    (void)signum;
     g_received_interrupt = 1;
 }
 
@@ -194,26 +248,38 @@ int shell(int argc, char *argv[]) {
             printf("%s", line);
         }
         //p.command = line;
-        vector *v;
+        vector *cmds;
+        int ret;
 EXECUTE:
-        v = parse(line);
-        const char *cmd = vector_get(v, 0);
-        if (strcmp(cmd, "exit") == 0) {
-            vector_destroy(v);
-            goto EXIT;
-        }
-        if (!vector_empty(v))
-            run_command(v, &next);
-        if (has_history && is_need_record(cmd)) {
-            if (fwrite(line, sizeof(char), strlen(line), output_file) != strlen(line)) {
-                fprintf(stderr, "%s\n", "write command into history.txt failed");
+        cmds = parse(line);
+        VECTOR_FOR_EACH(cmds, cmd, {
+            vector *v = split_cmd_args(cmd);
+            const char *cmd = vector_get(v, 0);
+            if (strcmp(cmd, "exit") == 0) {
+                vector_destroy(v);
+                goto EXIT;
             }
+            if (!vector_empty(v))
+               ret = run_command(v, &next);
+
+            vector_destroy(v);
+            // check to execute next command or not
+            if (vector_size(cmds) > 1 && ret != 0) {
+                if (strstr(line, "&&") != NULL) {
+                    break;
+                }
+            }
+        });
+        if (vector_size(cmds) == 1 && !is_need_record(vector_get(cmds, 0))) {
+            continue;
+        }
+
+        if (has_history && fwrite(line, sizeof(char), strlen(line), output_file)
+            != strlen(line)) {
+            fprintf(stderr, "%s\n", "write command into history.txt failed");
         }
         line[strlen(line)-1] = '\0';
-        if (is_need_record(cmd)) {
-            vector_push_back(g_list_command, line);
-        }
-        vector_destroy(v);
+        vector_push_back(g_list_command, line);
         memset(line, 0, line_len);
     }
 
